4d88dc2adbe8158eb27e9f29d9d3b33a8d67ba81
Aradhya R <nandeesharadhya19@gmail.com>
2025-03-12 18:15:17 +0530

    updated code

---
+++ week-2/code/ArrofObj.java
@@ -0,0 +1,49 @@
+class Student{
+
+        int rollno;
+        String name;
+        int marks;
+        
+}
+
+
+public class ArrofObj {
+    public static void main(String[] args) {
+        
+        //Creating 3 Objects for student
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+
+      
+        s1.rollno=1;
+        s1.name="Nandeesh";
+        s1.marks=98;
+
+        s2.rollno=1;
+        s2.name="Neha";
+        s2.marks=98;
+     
+        s3.rollno=1;
+        s3.name="Prajwal";
+        s3.marks=98;
+
+        Student students[] =new Student[3]; // Creating arroy of objects    // Refer arrayofobject.pdf
+        
+
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+      
+      //  students → [ (1, "Nandeesh", 98), (2, "Neha", 95), (3, "Prajwal", 90) ]
+      //                     students[0]        students[1]        students[0]
+       
+        for(int i=0;i<students.length;i++)
+       {
+            System.out.println(students[i].name+" "+students[i].rollno+" "+students[i].marks);
+       }
+
+       
+
+    }
+}
---
+++ week-2/code/DrawbacksofArray.java
@@ -0,0 +1,13 @@
+class DrawbacksofArray
+{
+    public static void main(String[] args)
+        {
+            System.out.println("Drawbacks of Array \n");
+            Gap.bar();
+            System.out.println("  Size of array is Irriversible");
+            System.out.println("  For any operation it Traverses all Element making it slow");
+            System.out.println("  Same datatype must be used for all elemnt in Array ");
+            System.out.println("  Collection overcomes Arrays");
+        }
+
+}
